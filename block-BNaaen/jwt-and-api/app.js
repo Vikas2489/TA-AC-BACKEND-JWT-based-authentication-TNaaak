14215a663530ac76ff58e2df4d12548cdcaf8473
vikas2489 <vk55002678@gmail.com>
2022-12-06 16:53:25 +0530

    finish API

--- block-BNaaen/jwt-and-api/app.js
+++ block-BNaaen/jwt-and-api/app.js
@@ -3,20 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require("mongoose");
-var dotenv = require("dotenv");
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var profilesRouter = require("./routes/profiles");
-var articlesRouter = require("./routes/articles");
+var profilesRouter = require('./routes/profiles');
+var articlesRouter = require('./routes/articles');
 
 dotenv.config();
 
 // connecting to db
-mongoose.connect("mongodb://localhost/users", (err) => {
+mongoose.connect(
+  'mongodb+srv://vikasdb:vikasdb@cluster0.e7ng15c.mongodb.net/?retryWrites=true&w=majority',
+  (err) => {
     return console.log(err ? err : true);
-});
+  }
+);
 
 var app = express();
 
@@ -36,19 +39,19 @@ app.use('/api/profiles', profilesRouter);
 app.use('/api/articles', articlesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
